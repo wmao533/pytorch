83311da7d7abe4963cc81ba3ababe85cf7499565
Taylor Robie <taylorrobie@fb.com>
2022-09-16 14:06:04 -0700

    [Profiler] Clean up Tensor representation
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/85161
    
    I want to start using `TensorMetadata` elsewhere in profiler so we have a common representation of Tensor. The main changes in this PR are:
    
    1) Replace raw pointers with strong typedefs and create a custom type caster to handle moving them to Python.
    2) Adding a `device()` method to handle reassembling type and index.
    3) Changing `dim_` from a 4 to an 8 byte integer. Before we captured TensorImpl and storage there were four int8 fields, so storing dim as a 32 bit int allowed the struct to pack into 8 bytes, rather than aligning to 16. Now the pointer alignment is going to force it to 32 bytes either way so narrowing doesn't buy us anything.
    ghstack-source-id: 167660683
    
    Differential Revision: [D39563965](https://our.internmc.facebook.com/intern/diff/D39563965/)

--- torch/csrc/profiler/python/init.h
+++ torch/csrc/profiler/python/init.h
@@ -1,6 +1,45 @@
 #pragma once
 
-#include <torch/csrc/utils/python_stub.h>
+#include <Python.h>
+#include <pybind11/pybind11.h>
+
+#include <torch/csrc/profiler/collection.h>
+#include <torch/csrc/utils/pybind.h>
+#include <torch/csrc/utils/python_numbers.h>
+
+namespace pybind11 {
+namespace detail {
+using torch::profiler::impl::StorageImplData;
+using torch::profiler::impl::TensorImplAddress;
+
+// Strong typedefs don't make much sense in Python since everything is duck
+// typed. So instead we simply cast them to ints, return them, and let the
+// caller handle correctness.
+template <typename T>
+struct strong_pointer_type_caster {
+  template <typename T_>
+  static handle cast(T_&& src, return_value_policy policy, handle parent) {
+    const auto* ptr = reinterpret_cast<const void*>(src.value_of());
+    return ptr ? handle(THPUtils_packUInt64(reinterpret_cast<intptr_t>(ptr)))
+               : none();
+  }
+
+  bool load(handle src, bool convert) {
+    return false;
+  }
+
+  PYBIND11_TYPE_CASTER(T, _("strong_pointer"));
+};
+
+template <>
+struct type_caster<StorageImplData>
+    : public strong_pointer_type_caster<StorageImplData> {};
+
+template <>
+struct type_caster<TensorImplAddress>
+    : public strong_pointer_type_caster<TensorImplAddress> {};
+} // namespace detail
+} // namespace pybind11
 
 namespace torch {
 namespace profiler {
